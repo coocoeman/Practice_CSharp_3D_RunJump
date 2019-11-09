e287cb45a2ecf221d29039fa1b5729bffd5b9c43
coocoeman <54761438+coocoeman@users.noreply.github.com>
2019-11-09 19:57:14 +0800

    控制跳躍

--- UnityProject/Assets/Boy.cs
+++ UnityProject/Assets/Boy.cs
@@ -6,6 +6,17 @@ public class Boy : MonoBehaviour
     public float speed = 1.5f;
     [Header("跳躍高度"), Range(100, 1500)]
     public int jump = 100;
+    
+    private Rigidbody r;
+    private Animator a;
+    private AudioSource aS;
+
+    private void Start()
+    {
+        aS = GetComponent<AudioSource>();
+        r = GetComponent<Rigidbody>();
+        a = GetComponent<Animator>();
+    }
 
     private void Update()
     {
@@ -15,5 +26,27 @@ public class Boy : MonoBehaviour
     private void Move()
     {
         transform.Translate(0, 0, speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision ==null)
+        {
+            return;
+        }
+        a.SetBool("跳躍開關",false);
+    }
+
+
+
+    public void Jump()
+    {
+        if (a.GetBool("跳躍開關"))
+        {
+            return;
+        }
+        r.AddForce(Vector3.up*jump);
+        a.SetBool("跳躍開關",true);
+        aS.Play();
     }
 }
